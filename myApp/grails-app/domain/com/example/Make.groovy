5157b1be23ab579f5ff3dd36682bd7a3572bee15
Xander Gerreman <xander@gerreman.nl>
2017-02-13 16:40:40 +0100

    init

---
+++ myApp/grails-app/domain/com/example/Make.groovy
@@ -0,0 +1,14 @@
+package com.example
+
+class Make {
+
+
+    String name
+
+    static constraints = {
+    }
+
+    String toString() {
+        name
+    }
+}
---
+++ myApp/grails-app/domain/com/example/Model.groovy
@@ -0,0 +1,14 @@
+package com.example
+
+class Model {
+
+    String name
+
+    static belongsTo = [ make: Make ]
+    static constraints = {
+    }
+
+    String toString() {
+        name
+    }
+}
