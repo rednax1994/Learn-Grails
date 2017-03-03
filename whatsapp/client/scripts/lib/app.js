dc9ef6464d60fe9e5097e9930c657797dd7d3b42
Xander Gerreman <xander@gerreman.nl>
2017-03-03 14:32:05 +0100

    playing

---
+++ whatsapp/client/scripts/lib/app.js
@@ -0,0 +1,32 @@
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
+import { Meteor } from 'meteor/meteor';
+
+import ChatsCtrl from '../controllers/chats.controller';
+import RoutesConfig from '../routes';
+
+const App = 'Whatsapp';
+
+Angular.module(App, [
+    'angular-meteor',
+    'ionic'
+]);
+
+new Loader(App)
+    .load(ChatsCtrl)
+    .load(RoutesConfig);
+
+if(Meteor.isCordova) {
+    Angular.element(document).on('deviceready', onReady);
+} else {
+    Angular.element(document).ready(onReady);
+}
+
+function onReady(){
+    Angular.bootstrap(document, [App]);
+}
